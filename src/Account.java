09fe7bd5efdcbf3d1f0a6fae127739e2e332d5fd
admin <tagiyev.z@gmail.com>
2021-06-28 00:45:18 -0700

    added

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+
+    long debitCardNO;
+    int debitCardPin;
+
+    public Account(long debitCardNO, int debitCardPin) {
+        this.debitCardNO = debitCardNO;
+        this.debitCardPin = debitCardPin;
+    }
+
+}
